79c13399be98142800684d3fe9d31166fea3ff95
jjosinoms@gmail.com <jjosinoms@gmail.com>
2018-03-23 13:02:58 -0300

    atualizacao teste

--- teste/settings.py
+++ teste/settings.py
@@ -119,3 +119,18 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+import dj_database_url
+DATABASES['default'] = dj_database_url.config()
+
+# Honor the 'X-Forwarded-Proto' header for request.is)secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# ALLOW all hosts headers
+ALLOWED_HOSTS = ['*']
+
+STATIC_ROOT = 'staticfiles'
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (os.path.join(BASE_DIR,' static'),)
+
